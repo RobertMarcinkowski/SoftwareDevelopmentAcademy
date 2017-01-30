c955e2afc0c61eaf337b3961e9b89959d787e4cd
Robert Marcinkowski <robertm1506@gmail.com>
2017-01-30 11:18:24 +0100

    added hangman project

---
+++ hangman/src/main/java/pl/sda/exercises/hangman/App.java
@@ -0,0 +1,13 @@
+package pl.sda.exercises.hangman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
